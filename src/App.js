83156ecb307e1db6888b5844246bccbddf5a3439
Le_Hanh <lengochanh.vtc@gmail.com>
2023-02-24 18:38:14 +0700

    clean code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeContext } from "./helper/ThemeContext.js";
 import { LIGHT_COLOR, DARK_COLOR } from "./constants";
 import TodoApp from "./Component/TodoApp";
 
 function App() {
-  const [theme, setTheme] = useState(() => {
-    const data = localStorage.getItem("theme");
-    if (data) {
-      return data;
-    } else {
-      return "light";
-    }
-  });
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
 
   useEffect(() => {
     document.querySelector("html").style =
